69d6f443f207cd1d20028d2287814892d0f799a1
suporte05 <suporte05@atsditsolutions.com>
2026-05-04 20:59:07 -0300

    chore: bootstrap spring boot project

---
+++ src/main/java/com/example/orderchallenge/OrderChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/orderchallenge/OrderChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
